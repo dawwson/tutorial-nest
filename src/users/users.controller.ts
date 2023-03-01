4f486cac57b9fed248c94778c7021b3670453767
dawwson <shines1427@gmail.com>
2023-03-02 00:03:10 +0900

    Feat: Users 컨트롤러 추가

---
+++ src/users/users.controller.ts
@@ -0,0 +1,34 @@
+import { Controller, Get, Post, Body, Param, Query } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { VerifyEmailDto } from './dto/verify-email.dto';
+import { UserLoginDto } from './dto/user-login.dto';
+import { UserInfo } from './UserInfo';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async createUser(@Body() dto: CreateUserDto): Promise<void> {
+    console.log(dto);
+  }
+
+  @Post('email-verify')
+  async verifyEmail(@Query() dto: VerifyEmailDto): Promise<string> {
+    console.log(dto);
+    return;
+  }
+
+  @Post('login')
+  async login(@Body() dto: UserLoginDto): Promise<string> {
+    console.log(dto);
+    return;
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') userId: string): Promise<UserInfo> {
+    console.log(userId);
+    return;
+  }
+}
